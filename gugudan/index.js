aa9a09f728786b31ec5556c1ae90321c4c30abe9
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-05-08 15:37:11 +0900

    add gugudan -ts

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,1 +1,17 @@
-console.log('gugudan');
+const resultText = document.querySelector('#result');
+
+document.querySelector('#click').addEventListener('click', () => {
+    const firstValue = document.querySelector('#first').value
+    const secondValue = document.querySelector('#second').value
+    if(firstValue){
+        if(secondValue){
+            const multipleResult = firstValue * secondValue
+            resultText.textContent = multipleResult
+        } else {
+            resultText.textContent = '두 번째 값을 입력하세요'
+        }
+    } else {
+        resultText.textContent = '첫 번째 값을 입력하세요'
+    }
+})
+
